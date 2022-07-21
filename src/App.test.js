867a785ea5e70aa9eaa4d2b448357f4895b253f2
Tushar Aggarwal <tushar.aggarwal@salesforce.com>
2022-07-20 22:50:15 -0700

    Adding tests

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders text correctly', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Loading/i);
-  expect(linkElement).toBeInTheDocument();
-});
